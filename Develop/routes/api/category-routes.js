e0a76d53365898bc9a07ec67877a60f61e9015ae
Javier Perez <javier21perez98@gmail.com>
2021-04-22 23:32:17 -0700

    created route to update categories

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -41,6 +41,22 @@ router.post("/", async (req, res) => {
 
 router.put("/:id", async (req, res) => {
 	// update a category by its `id` value
+	try {
+		const category = await Category.update(req.body, {
+			where: {
+				id: req.params.id,
+			},
+		});
+
+		if (!category) {
+			res.status(404).json({ message: "No location found with this id!" });
+			return;
+		}
+
+		res.status(200).json(category);
+	} catch (err) {
+		res.status(400).json(err);
+	}
 });
 
 router.delete("/:id", async (req, res) => {
