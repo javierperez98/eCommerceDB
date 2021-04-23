fb5c844a20cc4a115837705b4164f241bde703b8
Javier Perez <javier21perez98@gmail.com>
2021-04-22 23:34:57 -0700

    created route to delete categories and tested all category routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -61,6 +61,22 @@ router.put("/:id", async (req, res) => {
 
 router.delete("/:id", async (req, res) => {
 	// delete a category by its `id` value
+	try {
+		const category = await Category.destroy({
+			where: {
+				id: req.params.id,
+			},
+		});
+
+		if (!category) {
+			res.status(404).json({ message: "No location found with this id!" });
+			return;
+		}
+
+		res.status(200).json(category);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 module.exports = router;
